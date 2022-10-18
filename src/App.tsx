43f8c2961e995f1a8000c642a50ff29277b60cb0
osmanbeshirov <osman.beshirov@gmail.com>
2022-10-19 02:11:55 +0300

    default props

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 
 import './App.css';
 
@@ -9,10 +9,41 @@ const Heading = ({ title }: { title: string }) => {
   )
 }
 
+const HeadingWithContent = ({ children }: { children: ReactNode }): ReactElement => {
+  return <h1>{children}</h1>
+}
+
+
+//Default props
+const defaultContainerProps = {
+  heading: <strong>My Heading</strong>
+}
+
+type ContainerProps = {
+  children: ReactNode
+} & typeof defaultContainerProps
+
+const Container = ({
+  heading, children
+}: ContainerProps): ReactElement => {
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      <h1>{children}</h1>
+    </>
+  )
+}
+
+Container.defaultProps = defaultContainerProps;
+
+
 function App() {
   return (
     <div>
       <Heading title='Hi Guys' />
+      <HeadingWithContent>Hi I am a contant component</HeadingWithContent>
+      <Container>Foo</Container>
     </div>
   );
 }
